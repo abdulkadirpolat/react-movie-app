ca9d69dcb2864ef0007bc6cc463539c7c9907f0a
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-04 20:30:08 +0300

    Change has been made

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,19 +1,46 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // state = {
+  //   searchChange: "",
+  // };
+
+  formSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  // enterValue = (e) => {
+  //   if (e.key == "Enter") {
+  //     console.log("submit");
+  //     this.state.searchChange = "";
+  //     console.log(this.state.searchChange);
+  //   } else {
+  //     this.state.searchChange = "";
+  //   }
+  // };
+
   render() {
     return (
-      <input
-        className="my-3 w-100"
-        type={this.props.inputType}
-        placeholder="Search"
-      ></input>
+      <form onSubmit={this.formSubmit}>
+        <div className="form-row mb-5">
+          <div className="col-12">
+            <input
+              className="form-control"
+              type={this.props.inputType}
+              placeholder={this.props.placeHolder}
+              onChange={this.props.searchMovieProps}
+             // onKeyUp={this.enterValue}
+            ></input>
+          </div>
+        </div>
+      </form>
     );
   }
 }
 
 SearchBar.defaultProps = {
   inputType: "text",
+  placeHolder: "Searct",
 };
 
 export default SearchBar;

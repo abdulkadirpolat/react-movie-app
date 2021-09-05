5802f74f31ff7bc12af97740b2b365ff21b38320
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-05 23:08:52 +0300

    Bulma css framework imported

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./Components/App";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bulma/css/bulma.min.css";
 
 ReactDom.render(<App />, document.getElementById("root"));

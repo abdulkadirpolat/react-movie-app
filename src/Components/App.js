7f4493608d98f9763e76a85ccffebea14db3d560
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-05 23:10:52 +0300

    Corrections have been made

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,58 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
-
+import axios from "axios";
+require("dotenv").config();
 class App extends React.Component {
   state = {
-    movies: [
-      {
-        name: "The Matrix 3",
-        rating: "8.1",
-        overview:
-          "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg",
-        id: 1,
-      },
-      {
-        name: "The Matrix Reloaded",
-        rating: "6.9",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
-        overview:
-          "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, as more and more humans are being freed from the matrix and brought to Zion, the one and only stronghold of the Resistance. Neo himself has discovered his superpowers including super speed, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
-        id: 2,
-      },
-      {
-        name: "Saw 3D",
-        rating: "7.5",
-        overview:
-          "SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qHCZ6LjtmqWDfXXN28TlIC9OppK.jpg",
-        id: 3,
-      },
-      {
-        name: "Blitz 007",
-        rating: "8.6",
-        overview:
-          "A tough, renegade cop with a gay sidekick is dispatched to take down a serial killer who has been targeting police officers. AÇIKLAMA AÇIKLAMA",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qCPMjT8Ld8tvs1zs7LY2jpKlRIK.jpg",
-        id: 4,
-      },
-      {
-        name: "Hostage",
-        rating: "6.3",
-        imageURL:
-          "https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg",
-        overview:
-          "When a mafia accountant is taken hostage on his beat, a police officer – wracked by guilt from a prior stint as a negotiator – must negotiate the standoff, even as his own family is held captive by the mob.",
-        id: 5,
-      },
-    ],
+    movies: [],
     searchChange: "",
   };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://api.themoviedb.org/4/list/7106878?page=1&api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
+    );
+    this.setState({ movies: response.data.results });
+  }
 
   deleteMovie = (movie) => {
     const newMovieList = this.state.movies.filter(
@@ -71,24 +33,17 @@ class App extends React.Component {
   render() {
     let filterMov = this.state.movies.filter(
       (movie) =>
-        movie.name.toLowerCase().indexOf(this.state.searchChange) !== -1
+        movie.title.toLowerCase().indexOf(this.state.searchChange) !== -1
     );
     return (
-      <div className="container-xl">
-        <div className="row">
-          <div className="col-lg-12">
-            <SearchBar
-              inputType="text"
-              placeHolder="Search movies"
-              searchMovieProps={this.searchMovie}
-            />
-          </div>
-        </div>
-        <MovieList
-          deleteMovieProps={this.deleteMovie}
-          movies={filterMov}
-          className="row"
+      <div className="container">
+        <SearchBar
+          inputType="text"
+          placeHolder="Search movies"
+          searchMovieProps={this.searchMovie}
         />
+
+        <MovieList deleteMovieProps={this.deleteMovie} movies={filterMov} />
       </div>
     );
   }

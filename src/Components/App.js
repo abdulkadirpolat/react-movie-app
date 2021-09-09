382d3d87f0adfddbbbc35ab301c46ef6b7e2157e
Abdulkadir Polat <tr.abdulkadir.polat@gmail.com>
2021-09-09 20:39:18 +0300

    fixes
    fixes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+//import { useState, useEffect } from "react";
+import React from 'react'
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 import axios from "axios";
 require("dotenv").config();
+
 class App extends React.Component {
   state = {
     movies: [],
@@ -50,4 +52,45 @@ class App extends React.Component {
   }
 }
 
-export default App;
+
+// function App(props) {
+//   const [movies, setMovies] = useState([]);
+//   const [searchChange, setSearchChange] = useState("");
+
+//   useEffect(async () => {
+//     const response = await axios.get(
+//       `https://api.themoviedb.org/4/list/7106878?page=1&api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
+//     );
+//     setMovies({ movies: response.data.results });
+//   });
+
+//   const deleteMovie = (movie) => {
+//     const newMovieList = movies.filter((movi) => movi.id !== movie.id);
+
+//     setMovies({
+//       movies: newMovieList,
+//     });
+//   };
+
+//   const searchMovie = (e) => {
+//     setSearchChange({ searchChange: e.target.value.trim().toLowerCase() });
+//   };
+
+//   let filterMov = movies.filter(
+//     (movie) => movie.title.toLowerCase().indexOf(searchChange) !== -1
+//   );
+
+//   return (
+//     <div className="container px-5">
+//       <SearchBar
+//         inputType="text"
+//         placeHolder="Search movies"
+//         searchMovieProps={searchMovie}
+//       />
+
+//       <MovieList deleteMovieProps={deleteMovie} movies={filterMov} />
+//     </div>
+//   );
+// }
+
+  export default App;
